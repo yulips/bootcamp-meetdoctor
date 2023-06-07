2653e9fae1bd0024f238bb76386c9c7c569617cc
yulips <anayuliputri@gmail.com>
2023-06-07 19:21:05 +0700

    add laravel, jetstream, and migration

---
+++ database/migrations/2023_06_07_113057_create_permission_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('permission_id');
+            $table->integer('role_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_user');
+    }
+}
